7a9cd5c882feb15c78eb6170f436efd4730b3e4a
ajay14312 <ajaynitm10@gmail.com>
2020-03-04 21:02:48 +0530

    Update sumAll.js

--- js-exercises/sum-all/sumAll.js
+++ js-exercises/sum-all/sumAll.js
@@ -1,5 +1,6 @@
 function sumAll(arr) {
   let highestNum = arr[1] > arr[0] ? arr[1] : arr[0];
+  //I can do something like let highestNum = Math.max(arr) and let lowestNum = Math.min(arr). Use language built-in for simple things
   let lowestNum = arr[1] < arr[0] ? arr[1] : arr[0];
   let sum = 0;
 
